9af89e55b0f8aff0cc6f1c00d04457273a498aa5
Peter Fiee <peterfiee@icloud.com>
2023-09-03 01:49:15 -0400

    Added BMI classification info

--- bmi/app.py
+++ bmi/app.py
@@ -1,6 +1,26 @@
+import functools
 import gradio as gr
 
 
+def display_result(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        value = func(*args, **kwargs)
+        msg = ""
+        if value < 18.5:
+            msg += "Underweight"
+        elif 18.5 <= value <= 24.9:
+            msg += "Normal"
+        elif 25 <= value < 30:
+            msg += "Overweight"
+        else:
+            msg += "Obese"
+        return value, msg
+
+    return wrapper
+
+
+@display_result
 def calculate_bmi(height: float, weight: float) -> float:
     """calculate the body mass index given height and weight
 
@@ -15,5 +35,7 @@ def calculate_bmi(height: float, weight: float) -> float:
 
 
 if __name__ == "__main__":
-    interface = gr.Interface(fn=calculate_bmi, inputs=["number", "number"], outputs=["number"])
+    interface = gr.Interface(
+        fn=calculate_bmi, inputs=["number", "number"], outputs=["number", "text"]
+    )
     interface.launch()
